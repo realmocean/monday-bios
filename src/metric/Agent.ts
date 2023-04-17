6986f797991f88faf7a61646c0cb60d7eb4a328d
selimtan <stanoncloud@gmail.com>
2023-04-17 18:33:44 +0300

    .

--- src/metric/Agent.ts
+++ src/metric/Agent.ts
@@ -14,12 +14,34 @@ const SEND_ENDPOINT_THRESHOLD: number = 10;
 const MAX_ENDPOINT_LIMIT: number = 50;
 const MAX_SENDS: number = 60; // we send every minute, so this is sending for an hour.
 
+function getBrowserLocales(options = {}) {
+  const defaultOptions = {
+    languageCodeOnly: false,
+  };
+  const opt = {
+    ...defaultOptions,
+    ...options,
+  };
+  const browserLocales =
+    navigator.languages === undefined
+      ? [navigator.language]
+      : navigator.languages;
+  if (!browserLocales) {
+    return undefined;
+  }
+  return browserLocales.map(locale => {
+    const trimmedLocale = locale.trim();
+    return opt.languageCodeOnly
+      ? trimmedLocale.split(/-|_/)[0]
+      : trimmedLocale;
+  });
+}
 
 export class Agent {
 
   static defaults: any = {
     token: null,
-   // ingestUrl: "https://in.requestmetrics.com/v1",
+   //ingestUrl: "https://in.requestmetrics.com/v1",
     ingestUrl: "/realmocean/tracker:metric",
     monitorSelfCalls: false,
     tags: []
